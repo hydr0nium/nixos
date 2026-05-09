7af5502dabceaabe9aa1f3f1db7e578a3027aafc
hydr0nium <37932436+hydr0nium@users.noreply.github.com>
2026-05-09 13:15:31 +0200

    Fixed darkmode

--- home-manager/darkmode.nix
+++ home-manager/darkmode.nix
@@ -3,10 +3,30 @@
 {
     dconf.enable = true;
     dconf.settings = {
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-        gtk-theme = "Adwaita-dark";
-      };
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+        };
+    };
+    gtk = {
+        enable = true;
+        theme = {
+          name = "Orchis-Dark"; # Oder Adwaita-dark
+          package = pkgs.orchis-theme;
+        };
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+    }; 
+    qt = {
+        enable = true;
+        platformTheme.name = "adwaita"; # Oder "gtk"
+        style = {
+          name = "adwaita-dark";
+          package = pkgs.adwaita-qt;
+        };
     };
 
 }
